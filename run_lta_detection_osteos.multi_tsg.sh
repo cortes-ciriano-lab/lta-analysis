38215776d9cc50b70feedc08df9891195152e1d3
Ianthe <I.A.E.vanBelzen-2@prinsesmaximacentrum.nl>
2024-07-23 09:02:12 +0200

    cleanup

--- run_lta_detection_osteos.multi_tsg.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-SINGULARITYENV_R_MAX_VSIZE=150Gb
-singularity exec --bind /nfs/research/icortes/ /nfs/research/icortes/belzen/src/structural_variation_202405_amd.sif R -e "source('/nfs/research/icortes/belzen/src/TCGA_WGD_analysis/osteos.lta_detection.multi_tsg.conf');dataset_selection_label='osteos';source('/nfs/research/icortes/belzen/src/lta_detection.multi_tsg.R')"
-
